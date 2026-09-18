222522fb62e829c12a59ba8bd8c2b2202ebefe43
raisircl <rai.verma@live.com>
2026-09-18 22:24:38 +0530

    modules

---
+++ sysmodule.py
@@ -0,0 +1,14 @@
+import sys
+
+#print(sys.argv)  # when we pass the data from cli that accep in sys module##
+
+for item in sys.argv:
+    print(item)
+
+# bydefault argv has one value which is the script name itself
+
+
+#print(sys.version)
+#print(sys.platform)
+#print(sys.executable)
+#print(sys.path)
